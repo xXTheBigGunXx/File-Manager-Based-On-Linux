c83ac2f188450c10041de563520ab0184e1a6591
xXTheBigGunXx <144613981+xXTheBigGunXx@users.noreply.github.com>
2025-03-13 23:38:01 +0200

    Final commit! Not

--- commandImplementation.cpp
+++ commandImplementation.cpp
@@ -5,8 +5,11 @@ bool Commands::RemoveFiles(const CommandLineData& CData)
     for(size_t i = 1; i < CData.GetLength(); i++)
     {
         const std::string filesPath = CData.GetArgument(i);
+        std::error_code er;
 
-        if(std::filesystem::remove(filesPath) == false || Authentication::IsTextFile(filesPath) == false)
+        if(std::filesystem::remove(filesPath, er) == false)
+            std::cout << "Filesystem library throws error: "<<er.message()<<'\n';
+        else if (Authentication::IsTextFile(filesPath) == false)
             std::cout << "Can't remove a file named: "<< filesPath << '\n'; 
     }
     return true;
@@ -18,14 +21,17 @@ bool Commands::CreateFile(const CommandLineData& CData)
     {
         const std::string filesName = CData.GetArgument(i);
         std::ofstream file(filesName);
-
-        if(file.is_open() == false || Authentication::IsTextFile(filesName) == false)
-        {
-            std::filesystem::remove(filesName);
-            std::cout << "Can't create a file: " << filesName << '\n'; 
-        }
-
         file.close();
+
+        if(Authentication::IsTextFile(filesName) == false)
+        {
+            std::cout << "Can't create a file: " << filesName << '\n';
+            std::error_code ec;
+            if(std::filesystem::remove(filesName, ec))
+            {
+                std::cout << "Filesystem library throws error: "<<ec.message()<<'\n';
+            }  
+        }
     }
     return true;
 }
@@ -112,15 +118,23 @@ bool Commands::PrintContent(const CommandLineData& CData)
     if(CData.GetLength() == 2)
     {
         std::string filesPath = CData.GetArgument(1);
+
         if(!Authentication::IsTextFile(filesPath))
             return false;
 
         std::ifstream file(filesPath);
 
-        while(file.is_open() == true && std::getline(file, filesPath))
+        if(file.is_open() == false)
+        {
+            std::cout << "Cannot read file: " << filesPath << '\n';
+            return false;
+        }
+
+        while(std::getline(file, filesPath))
         {
             std::cout << filesPath << '\n';
         }
+        file.close();
         return true;
     }
     else if(CData.GetLength() == 3)
@@ -128,77 +142,130 @@ bool Commands::PrintContent(const CommandLineData& CData)
         std::ofstream file;
 
         if(CData.GetArgument(1).compare(">>") == 0)
-        {
-            CommandLineData temp(CData);
-            temp.ChangeToRead(1, ">");
-            PrintContent(temp);
             file.open(CData.GetArgument(2), std::ios::app);
-        }
-        else if (std::string(CData.GetArgument(1)).compare(">") == 0)
-        {
+        else if (CData.GetArgument(1).compare(">") == 0)
             file.open(CData.GetArgument(2), std::ios::trunc);
-        }
         else
         {
-            throw std::runtime_error("Error!");
+            std::cout << "Append mode is not specified: " << CData.GetArgument(1) << '\n';
+            return false;
         }
+
+        CommandLineData temp{CData};
+        temp.ChangeToRead(1, temp.GetArgument(2));
+        PrintContent(temp);
 
         std::string line;
         while(std::getline(std::cin, line))
-        {
-            if(line.find("^D") != std::string::npos)
-                return false;
             file << line << '\n';
-        }
         
         file.close();
         return true;
     }
-    else
-    {
-        throw std::runtime_error("Erro!");
-    }
+    std::cout << "Not enought arguments for the command.\n";
+    return false;
 }
 
 bool Commands::MakeDirectory(const CommandLineData& CData)
 {
-    if(CData.GetLength() != 2)
-        throw std::runtime_error("!!!");
-    return std::filesystem::create_directories(CData.GetArgument(1));   
+    if(!Authentication::CommandLineCheck(2, CData.GetLength()))
+        return false;
+
+    std::error_code er;
+    if(std::filesystem::create_directories(CData.GetArgument(1), er) == false)
+    {
+        std::cout << "Filesystem library throws error: "<<er.message()<<'\n';
+        return false;
+    }
+    return true;
 }
 
 bool Commands::RemoveDirectory(const CommandLineData& CData)
 {
     if(CData.GetLength() != 2)
-        throw std::runtime_error("!!!");
-    return std::filesystem::remove(CData.GetArgument(1));
+    {
+        std::cout << "Not enought arguments for the command.\n";
+        return false;
+    }
+    std::error_code er;
+    if(std::filesystem::remove_all(CData.GetArgument(1), er) == false)
+    {
+        std::cout << "Filesystem library throws error: "<<er.message()<<'\n';
+        return false;
+    }
+    return true;
 }
 
 bool Commands::ListFiles(const CommandLineData& CData)
 {
-    const std::filesystem::path filesPath = std::filesystem::current_path();
-    for(const auto& i : std::filesystem::directory_iterator(filesPath))
+    if(CData.GetLength() == 1)
     {
-        std::cout << i.path() << '\n';
+        const std::filesystem::path filesPath = std::filesystem::current_path();
+        for(const auto& i : std::filesystem::directory_iterator(filesPath))
+        {
+            std::cout << i.path() << '\n';
+        }
+        return true;
     }
-    return true;
+    else if (CData.GetLength() == 2)
+    {
+        const std::string filesFormat = CData.GetArgument(1);
+        if(!Authentication::IsTextFile(filesFormat))
+        {
+            std::cout << "Listing of this file is not supported: "<< filesFormat <<"\n";
+            return false;
+        }
+
+        const std::filesystem::path filesPath = std::filesystem::current_path();
+
+        for(const auto& i : std::filesystem::directory_iterator(filesPath))
+        {
+            const std::string filesPath = i.path().string();
+            const size_t CIndexOfDot = filesPath.rfind('.');
+
+            if(CIndexOfDot == filesPath.length() || CIndexOfDot == std::string::npos)
+                continue;
+            
+            const std::string filesType = filesPath.substr(CIndexOfDot);
+
+            if(filesType.compare(filesFormat) == 0)
+            {
+                std::cout << i.path() << '\n';
+            }
+        }
+        return true;
+    }
+    else{
+        std::cout << "Not enought arguments for the command.\n";
+        return false;
+    }
 }
 
 bool Commands::FindFile(const CommandLineData& CData)
 {
     if(CData.GetLength() != 4)
-        throw std::runtime_error("!!!");
-    
+    {
+        std::cout << "Not enought arguments for the command.\n";
+        return false;   
+    }
+
     std::filesystem::path directPath = CData.GetArgument(1);
     std::string options = CData.GetArgument(2);
     std::string filesName = CData.GetArgument(3);
 
-    if (!std::filesystem::exists(directPath) || !std::filesystem::is_directory(directPath))
-        throw std::runtime_error("Invalid directory!");
-
-    if(directPath.compare(".") == 0 || options[0] == '-')
+    if(directPath.compare(".") == 0)
     {
         directPath = std::filesystem::current_path();
+    }
+    if (!std::filesystem::exists(directPath) || !std::filesystem::is_directory(directPath))
+    {
+        std::cout << "Directories name is invalid: " << directPath << '\n';
+        return false;
+    }
+    if (options.at(0) != '-')
+    {
+        std::cout << "Invalid second argument: " << options << '\n';
+        return false;
     }
 
     const std::string name = LoopDirectory(directPath, filesName);
@@ -224,7 +291,7 @@ std::string Commands::LoopDirectory(std::filesystem::path& directoriesPath, cons
             if (!result.empty())
                 return result;
         }
-        else if (std::filesystem::is_regular_file(filesOrDirecPath))
+        else if (std::filesystem::is_regular_file(filesOrDirecPath) && Authentication::IsTextFile(filesOrDirecPath.string()))
         {
             if (filesOrDirecPath.filename().string() == fileName)
                 return filesOrDirecPath.string();
--- commands.md
+++ commands.md
@@ -1,11 +1,21 @@
 
-Linux file management and file manipulation commands (all of these commands need to have the "xx" prefix):
+    
+    Linux file management and file manipulation commands (all of these commands need to have the "xx" prefix):
 
--touch <filename1> <filename2> <filename3> ... - command is used to create one or multiple files to the currect directory.
--rm <filename1> <filename2> <filename3> ... - command is used to delete one or multiple files from a currect directory.
--echo <message> <append mode> <filesname> - command is used to create and write the message directly into a file (append mode is written is square brackets).
--mv:
-    * mv <filesname1> <filename2> <filename3> .. <directoriesPath> - command is used to move one or multiple files to a directory.
-    * mv <filesname1> <filesname2> - command is used to rename <filesname1> name to <filesname2> name.
 
-.
+    -touch <filename1> <filename2> <filename3> ... - command is used to create one or multiple files to the currect directory.
+    -rm <filename1> <filename2> <filename3> ... - command is used to delete one or multiple files from a currect directory.
+    -echo <message> <append mode> <filesname> - command is used to create and write the message directly into a file (append mode is written is square brackets).
+    -mv:
+        * mv <filesname1> <filename2> <filename3> .. <directoriesPath> - command is used to move one or multiple files to a directory.
+        * mv <filesname1> <filesname2> - command is used to rename <filesname1> name to <filesname2> name.
+    -cat:
+        * cat <filesname> - command is used to print out to the console files content.
+        * cat <appendMode> <filename> - command is used to rewrite or append text to a file. (">>" - is append mode and ">" - truncate mode.(append mode command is written with duoble quotes - "")).
+    -mkdir <directoriesPath> - command is used to create single or nested directories.
+    -rmdir <directoriesPath> - command is used to delete the directory and its content.
+    -ls:
+        * ls - command is used to print current directories content.
+        * ls <filesFormat> - command is used to print only the certaint type of files in a current directory.
+    -find <directoriesPath> <constantVariable> <filename> - command is used to find the file in specified directory. (directoriesPath is '.' searching is in current path. constantVariable should always have dash in front of its have).
+
